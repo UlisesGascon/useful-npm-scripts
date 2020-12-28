56a8d9e05190d6e67feebdd6be5b40aaac886ea6
Ulises Gascon <ulises.gascon@guidesmiths.com>
2020-12-28 17:36:24 +0100

    chore: linted files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const server = require('./server')
+const port = process.env.PORT || 5000
 
 server.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`)
+  console.log(`Server listening at http://localhost:${port}`)
 })
--- src/server.js
+++ src/server.js
@@ -1,14 +1,13 @@
 const express = require('express')
 const server = express()
-const port = process.env.PORT || 5000
 const { name, version } = require('../package.json')
 
 server.get('/__/health', (req, res) => {
-  res.json({status: "ok"})
+  res.json({ status: 'ok' })
 })
 
 server.get('/__/manifest', (req, res) => {
-  res.json({name, version})
+  res.json({ name, version })
 })
 
 module.exports = server
